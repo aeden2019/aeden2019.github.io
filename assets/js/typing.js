cb2799f6347f34a01473c65238adeda9dda8384f
Andrew Eden <113564277+aeden2019@users.noreply.github.com>
2024-02-14 12:25:59 -0500

    Cleaned up typing.js

--- assets/js/typing.js
+++ assets/js/typing.js
@@ -1,35 +1,5 @@
-// // Typing effect for the text
-// var text = "Now I want the cursor to always blink.";
-// var index = 0;
-// var cursorVisible = true; // Initial cursor state
-
-// function type() {
-//     var cursor = cursorVisible ? '|' : ''; // Determine cursor visibility
-//     if (index <= text.length) {
-//         document.getElementById("typing-text").innerHTML = text.substring(0, index) + cursor;
-//         index++;
-//         cursorVisible = !cursorVisible; // Toggle cursor visibility
-//         setTimeout(type, 100); // Adjust typing speed here (milliseconds)
-//     } else {
-//         setInterval(toggleCursor, 500); // Start blinking cursor
-//     }
-// }
-
-// function toggleCursor() {
-//     cursorVisible = !cursorVisible; // Toggle cursor visibility
-//     var cursor = cursorVisible ? '|' : ''; // Determine cursor visibility
-//     document.getElementById("typing-text").innerHTML = text + cursor;
-// }
-
-// // Start typing animation with delay when the page loads
-// window.onload = function() {
-//     setTimeout(function() {
-//         type();
-//     }, 500); // Adjust delay here (milliseconds)
-// }
-
 // Typing effect for the text
-var text = "How about we remove that weird delay?";
+var text = "Hi, my name is Andrew.";
 var index = 0;
 var cursorVisible = true; // Initial cursor state
 var cursorInterval; // Store the interval ID
