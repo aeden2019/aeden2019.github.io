389d0caa337781e441f3177bce44e809582daff7
Andrew Eden <113564277+aeden2019@users.noreply.github.com>
2024-02-14 11:44:22 -0500

    Update typing.js

--- assets/js/typing.js
+++ assets/js/typing.js
@@ -1,5 +1,27 @@
+// // Typing effect for the text
+// var text = "Hi, my name is Andrew.";
+// var index = 0;
+// var cursorVisible = true;
+
+// function type() {
+//     if (index <= text.length) { 
+//         var cursor = cursorVisible ? '|' : '';
+//         document.getElementById("typing-text").innerHTML = text.substring(0, index) + cursor;
+//         cursorVisible = !cursorVisible;
+//         index++;
+//         setTimeout(type, 100); // Adjust typing speed here (milliseconds)
+//     }
+// }
+
+// // Start typing animation with delay when the page loads
+// window.onload = function() {
+//     setTimeout(function() {
+//         type();
+//     }, 500); // Adjust delay here (milliseconds)
+// }
+
 // Typing effect for the text
-var text = "Hi, my name is Andrew.";
+var text = "Time to test something new. ";
 var index = 0;
 var cursor = '|'; // Cursor character
 
@@ -17,5 +39,3 @@ window.onload = function() {
         type();
     }, 500); // Adjust delay here (milliseconds)
 }
-
-
