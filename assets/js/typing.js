1591337fb247794b7d522ab4325cd7afd902e343
Andrew Eden <113564277+aeden2019@users.noreply.github.com>
2024-02-14 11:07:18 -0500

    Slowed down typing effect

--- assets/js/typing.js
+++ assets/js/typing.js
@@ -6,7 +6,7 @@ function type() {
     if (index < text.length) {
         document.getElementById("typing-text").innerHTML += text.charAt(index);
         index++;
-        setTimeout(type, 100); // Adjust typing speed here (milliseconds)
+        setTimeout(type, 200); // Adjust typing speed here (milliseconds)
     }
 }
 
