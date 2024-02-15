f30dc937af62f2d24cf152805a15e1cd9e9c1f63
Andrew Eden <113564277+aeden2019@users.noreply.github.com>
2024-02-14 21:55:07 -0500

    Cleaned latest typing.js version

--- assets/js/typing.js
+++ assets/js/typing.js
@@ -1,96 +1,3 @@
-// // Typing effect for the text
-// var text = "Hi, my name is Andrew.";
-// var index = 0;
-// var cursorVisible = true; // Initial cursor state
-// var cursorInterval; // Store the interval ID
-
-// function type() {
-//     var cursor = cursorVisible ? '|' : ''; // Determine cursor visibility
-//     if (index <= text.length) {
-//         document.getElementById("typing-text").innerHTML = text.substring(0, index) + cursor;
-//         index++;
-//         cursorVisible = !cursorVisible; // Toggle cursor visibility
-//         setTimeout(type, 100); // Adjust typing speed here (milliseconds)
-//     } else {
-//         cursorInterval = setInterval(toggleCursor, 500); // Start blinking cursor
-//     }
-// }
-
-// function toggleCursor() {
-//     cursorVisible = !cursorVisible; // Toggle cursor visibility
-//     var cursor = cursorVisible ? '|' : ''; // Determine cursor visibility
-//     document.getElementById("typing-text").innerHTML = text + cursor;
-// }
-
-// // Start typing animation with delay when the page loads
-// window.onload = function() {
-//     setTimeout(function() {
-//         type();
-//     }, 500); // Adjust delay here (milliseconds)
-// }
-
-// // Clear the cursor interval when the page unloads
-// window.onunload = function() {
-//     clearInterval(cursorInterval);
-// }
-
-
-
-
-// var text = "Hi, my name is Burger.";
-// var newText = "Hi, my name is Andrew.";
-// var index = 0;
-// var cursorVisible = true;
-// var cursorInterval;
-
-// function type() {
-//     var cursor = cursorVisible ? '|' : '';
-//     if (index <= text.length) {
-//         document.getElementById("typing-text").innerHTML = text.substring(0, index) + cursor;
-//         index++;
-//         cursorVisible = !cursorVisible;
-//         setTimeout(type, 100);
-//     } else {
-//         replaceText(); 
-//     }
-// }
-
-// function replaceText() {
-//     var currentIndex = text.indexOf("Burger");
-//     if (currentIndex >= 0) {
-//         var removedText = text.substring(0, currentIndex) + text.substring(currentIndex + 1);
-//         document.getElementById("typing-text").innerHTML = removedText;
-//         text = removedText;
-//         setTimeout(replaceText, 100); // Adjust typing speed here
-//     } else if (text === "Hi, my name is ") {
-//         index = 0; // Reset the index for typing "Andrew"
-//         text = newText; // Set text to "Hi, my name is Andrew."
-//         setTimeout(type, 500); // Start typing "Andrew" after a delay
-//     } else {
-//         text = "Hi, my name is "; // Start removing "Burger" from the end
-//         setTimeout(replaceText, 500); // Wait before starting to remove "Burger"
-//     }
-// }
-
-// function toggleCursor() {
-//     cursorVisible = !cursorVisible;
-//     var cursor = cursorVisible ? '|' : '';
-//     document.getElementById("typing-text").innerHTML = text + cursor;
-// }
-
-// window.onload = function() {
-//     setTimeout(function() {
-//         type();
-//     }, 500); // Adjust delay here
-// }
-
-// window.onunload = function() {
-//     clearInterval(cursorInterval);
-// }
-
-
-
-
 var startText = "Hi, my name is Burger";
 var endText = "Hi, my name is Andrew.";
 var replacement = "Burger"
@@ -100,7 +7,6 @@ var cursorInterval;
 var cursor = cursorVisible ? '|' : '';
 
 function type() {
-    // var cursor = cursorVisible ? '|' : '';
     // Iterate through the full text
     if (index <= startText.length) {
         document.getElementById("typing-text").innerHTML = startText.substring(0, index) + cursor;
@@ -109,32 +15,24 @@ function type() {
         setTimeout(type, 100);
     // Once the first text is written, run the replacement function
     } else {
-        console.log("Moving to removeText with index: " + index);
-        //removeText(cursor); 
         removeText();
     }
 }
 
-// function removeText(cursor) {
 function removeText() {
     var targetIndex = startText.indexOf(replacement); // Get the location of the word to replace
-    console.log("Target index of: " + targetIndex);
     // Iterate backwards until the replacement index
     if (index >= targetIndex) {
-        console.log("Current index " + index);
         document.getElementById("typing-text").innerHTML = startText.substring(0, index) + cursor;
-        console.log("Current text: " + startText.substring(0, index))
         index--;
         cursorVisible = !cursorVisible;
         setTimeout(removeText, 100);
     } else {
         console.log("Move to replaceText with index: " + index);
-        //replaceText(cursor);
         replaceText();
     }
 }
 
-// function replaceText(cursor) {
 function replaceText() {
     // Iterate forward through the new word    
     if (index <= endText.length) {
